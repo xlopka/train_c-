e823b76c1222db155029df984caa1b0488c05af2
xlopka <88017417+xlopka@users.noreply.github.com>
2023-02-02 16:31:32 +0700

    Update project.cpp

--- project.cpp
+++ project.cpp
@@ -1,35 +1,34 @@
 #include <stdio.h>
 int points = 0;
-bool victory(int need) {
+bool victory(int need){
     if (need == points) return true;
     else return false;
 }
-void message() {
+void message(){
     int answer;
-    scanf("%d", &answer);
-    if (answer == 10) {
+    scanf("%d",&answer);
+    if (answer == 10){
         points++;
-        printf("Верно!\n");
+        printf("Р’РµСЂРЅРѕ!\n");
     }
-    else printf("Ошибка\n");
+    else printf("РћС€РёР±РєР°\n");
 }
-void primer(int i) {
-    int a[10] = { 1,2,3,4,5,6,7,8,9,10 };
-    int b[10] = { 9,8,7,6,5,4,3,2,1,0 };
-    printf("%d + %d = ", a[i], b[i]);
+void primer(int i){
+    int a[10] = {1,2,3,4,5,6,7,8,9,10};
+    int b[10] = {9,8,7,6,5,4,3,2,1,0};
+    printf("%d + %d = ",a[i],b[i]);
     message();
-
+    
 }
-int main() {
-    printf("Сколько очков для завершения тренировки?");
+int main(){
+    printf("РЎРєРѕР»СЊРєРѕ РѕС‡РєРѕРІ РґР»СЏ Р·Р°РІРµСЂС€РµРЅРёСЏ С‚СЂРµРЅРёСЂРѕРІРєРё?");
     int need;
-    scanf("%d", &need);
+    scanf("%d",&need);
     int attemps = 0;
-    for (int i = 0; points != need; i++) {
+    for (int i = 0; victory(need) != true; i++){
         attemps++;
         primer(i);
-        victory(need);
     }
-    printf("Тренировка завершена!\nВерных ответов %d из %d", points, attemps);
-
+    printf("РўСЂРµРЅРёСЂРѕРІРєР° Р·Р°РІРµСЂС€РµРЅР°!\nР’РµСЂРЅС‹С… РѕС‚РІРµС‚РѕРІ %d РёР· %d",points,attemps);
+    return 0;
 }
